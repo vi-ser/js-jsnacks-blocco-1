fc70078cb4edf96dbfffa154efaca658b12829b1
vi_ser <vinceulisse@gmail.com>
2024-02-02 14:03:17 +0100

    Snack 4 concluso

--- jsnack4/js/script.js
+++ jsnack4/js/script.js
@@ -12,10 +12,34 @@ const outputSumElement = document.querySelector("#output-sum");
 let sum = 0;
 
 // chiedo all'utente di inserire un numero di 4 cifre
-const number = Number(prompt("Inserisci un numero intero di 4 cifre"));
+let numberPrompt = Number(prompt("Inserisci un numero intero di 4 cifre"));
 
-// calcolo le singole cifre dividendole per migliaia, centinaia e decine
-let firstDigit = Math.floor(number / 1000);
+// questa variabile la uso per tenere il valore di numberPrompt salvato in modo da mandarlo in output alla fine
+let numberTemp = numberPrompt;
+
+// calcolo le singole cifre dividendole per migliaia, centinaia, decine e unità
+let firstDigit = Math.floor(numberTemp / 1000);
 console.log(firstDigit);
 
+// sottraggo le migliaia (moltiplicate per la cifra) e calcolo le centinaia
+numberTemp = numberTemp - firstDigit * 1000;
+let secondDigit = Math.floor(numberTemp / 100);
+console.log(secondDigit);
 
+// sottraggo le centinaia (moltiplicate per la cifra) e calcolo le decine
+numberTemp = numberTemp - secondDigit * 100;
+let thirdDigit = Math.floor(numberTemp / 10);
+console.log(thirdDigit);
+
+// sottraggo le decine (moltiplicate per la cifra) e calcolo le unità
+numberTemp = numberTemp - thirdDigit * 10;
+let fourthDigit = Math.floor(numberTemp);
+console.log(fourthDigit);
+
+// Oddio come ho fatto??? Sto amando la programmazione
+
+// calcolo la somma delle cifre
+sum = firstDigit + secondDigit + thirdDigit + fourthDigit;
+
+// stampo il risultato
+outputSumElement.innerHTML = `Hai inserito: ${numberPrompt}, la somma delle cifre è ${sum}`;
